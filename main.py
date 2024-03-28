176162b501df0c907582f440c46304e180cfefe1
sumo-boi <kozhushko2006@gmail.com>
2024-03-29 00:24:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import cv2
-from pymongo import MongoClient
 from simple_facerec import SimpleFacerec
 
 # Распознование базы наших лиц
@@ -9,23 +8,21 @@ sfr.load_encoding_images("images/")
 # Загрузка камеры
 cap = cv2.VideoCapture(0)
 
-
 while True:
     ret, frame = cap.read()
 
-# Распознание персон (лиц)
+    # Распознание персон (лиц)
     face_locations, face_names = sfr.detect_known_faces(frame)
     for face_loc, name in zip(face_locations, face_names):
         y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
 
-        cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2 )
+        cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
-
 
     cv2.imshow("Frame", frame)
 
     key = cv2.waitKey(1)
-    if key==27:
+    if key == 27:
         break;
 
 cap.release()
